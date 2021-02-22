1bab784ff089faa71a1178083722fe03b1558b07
Habib Siddiqui <habib.my@rediffmail.com>
2021-02-22 22:13:12 +0530

    generate session string

--- app/generate_sessionstring.py
+++ app/generate_sessionstring.py
@@ -6,11 +6,11 @@ api_hash = os.environ.get("API_HASH")
 self = Client("my_account", api_id, api_hash)
 
 with self:
-    print(self.export_session_string())
+    print("self",self.export_session_string())
 
 bot_name = os.environ.get("BOT_NAME")
 bot_tkn = os.environ.get("BOT_TKN")
 
 bot = Client(bot_name,bot_token=bot_tkn)
 with bot:
-    print(bot.export_session_string())
+    print("bot",bot.export_session_string())
