3b3dca164e9e80548314ea3fc0a22a69e9c1c0a7
Habib Siddiqui <habib.my@rediffmail.com>
2021-02-22 22:20:15 +0530

    genrate session string

--- app/generate_sessionstring.py
+++ app/generate_sessionstring.py
@@ -3,10 +3,6 @@ import os
 
 api_id = os.environ.get("API_ID")
 api_hash = os.environ.get("API_HASH")
-self = Client("my_account", api_id, api_hash)
-
-with self:
-    print("self",self.export_session_string())
 
 bot_name = os.environ.get("BOT_NAME")
 bot_tkn = os.environ.get("BOT_TKN")
@@ -14,3 +10,9 @@ bot_tkn = os.environ.get("BOT_TKN")
 bot = Client(bot_name,bot_token=bot_tkn)
 with bot:
     print("bot",bot.export_session_string())
+    
+self = Client("my_account", api_id, api_hash)
+
+with self:
+    print("self",self.export_session_string())
+
