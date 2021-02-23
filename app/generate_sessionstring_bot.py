3f61d532c58ec006e314b808798c5829c01e90c8
Habib Siddiqui <habib.my@rediffmail.com>
2021-02-23 13:06:31 +0530

    session string generator updated

--- app/generate_sessionstring_bot.py
+++ app/generate_sessionstring_bot.py
@@ -5,6 +5,9 @@ bot_name = os.environ.get("BOT_NAME")
 bot_tkn = os.environ.get("BOT_TKN")
 
 bot = Client(bot_name,bot_token=bot_tkn)
-with bot:
-    print("bot",bot.export_session_string())
 
+s_file = bot_name + ".session"
+
+with open(s_file,"w") as f:
+    with bot:
+        print("bot",bot.export_session_string())
--- generate_sessionstring.py
+++ generate_sessionstring.py
@@ -6,5 +6,9 @@ api_hash = os.environ.get("API_HASH")
 
 self = Client("my_account", api_id, api_hash)
 
-with self:
-    print("self",self.export_session_string())
+s_file = "my_account.session"
+
+with open(s_file,"w") as f:
+    with self:
+        print(self.export_session_string(),file=f)
+
